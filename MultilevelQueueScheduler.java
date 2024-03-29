f174d0226f78c3b5c22aff8b9a28ea550e7a5244
joud alnayel <146003258+joudalnayel@users.noreply.github.com>
2024-03-29 03:25:54 +0300

    Update MultilevelQueueScheduler.java

--- MultilevelQueueScheduler.java
+++ MultilevelQueueScheduler.java
@@ -1,62 +1,66 @@
 import java.util.*;
 
 public class MultilevelQueueScheduler {
-
+	
     public static void main(String[] args) {
+    	
         Scanner kb = new Scanner(System.in);
-        int priority,arrivalTime,cpuBurstTime,ProcessID,startTime,terminationTime,turnAroundTime,waitingTime,responseTime;
+        int choice,numProcesses,priority,arrivalTime,burstTime;
+        String processID;
+        List<PCB> Q1 = new ArrayList<>();
+        List<PCB> Q2 = new ArrayList<>();
+         
+        do {
+            System.out.println("Multilevel Queue Scheduling Algorithm");
+            System.out.println("1. Enter process information.");
+            System.out.println("2. Report detailed information about each process and different scheduling criteria.");
+            System.out.println("3. Exit the program.");
+            System.out.print("Enter your choice: ");
+            choice = kb.nextInt();
 
-        // The program will create two arrays of type PCB to represent Q1 and Q2:
-        List<ProcessControlBlock> q1 = new ArrayList<>();
-        List<ProcessControlBlock> q2 = new ArrayList<>();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter the total number of processes in the system: ");
+                    numProcesses = kb.nextInt();
 
-        // The program will prompt the user to enter the number of processes (P):
-        System.out.print("Enter the number of processes: ");
-        int numProcesses = kb.nextInt();
+                    for (int i = 1; i <= numProcesses; i++) {
+                        System.out.println("Enter information for Process " + i + ":");
+                        System.out.print("Process ID: ");
+                        processID = kb.next();
 
-        // The program will prompt the user to enter the priority, arrival time, and CPU burst of each process:
-        
-        for (int i = 1; i <= numProcesses; i++) {
-            System.out.println("Enter process "+i+" information:");
-            System.out.print("Priority (1 or 2): ");
-            priority = kb.nextInt();
-            System.out.print("Arrival time: ");
-            arrivalTime = kb.nextInt();
-            System.out.print("CPU burst time: ");
-            cpuBurstTime = kb.nextInt();
-        }
-        
-        // The program will create a process (object) of type PCB for each process and initialize its attributes: 
-        for (int i = 1; i <= numProcesses; i++) {
-            System.out.println("Enter process " + i + " information:");
-            System.out.print("Please enter the Process ID: ");
-            ProcessID = kb.nextInt();
-            System.out.print("Please enter the Priority (1 or 2): ");
-            priority = kb.nextInt();
-            System.out.print("Please enter the Arrival time: ");
-            arrivalTime = kb.nextInt();
-            System.out.print("Please enter the CPU burst time: ");
-            cpuBurstTime = kb.nextInt();
-            System.out.print("Please enter the start time: ");
-            startTime = kb.nextInt();
-            System.out.print("Please enter the termination time: ");
-            terminationTime = kb.nextInt();
-            System.out.print("Please enter the Turn around time: ");
-            turnAroundTime = kb.nextInt();
-            System.out.print("Please enter the Waiting time: ");
-            waitingTime = kb.nextInt();
-            System.out.print("Please enter the Response time: ");
-            responseTime = kb.nextInt();
+                        System.out.print("Priority (1 or 2): ");
+                        priority = kb.nextInt();
 
-            ProcessControlBlock pcb = new ProcessControlBlock(ProcessID, priority, arrivalTime, cpuBurstTime, startTime, terminationTime, turnAroundTime, waitingTime, responseTime);
+                        System.out.print("Arrival time: ");
+                        arrivalTime = kb.nextInt();
 
-            if (priority == 1) {
-                q1.add(pcb);
-            } else if (priority == 2) {
-                q2.add(pcb);
-            } else {
-                System.out.println("Invalid priority. Process not added.");
+                        System.out.print("CPU burst time: ");
+                        burstTime = kb.nextInt();
+
+                        PCB process = new PCB(processID, priority, arrivalTime, burstTime);
+
+                        if (priority == 1) {
+                            Q1.add(process);
+                        } else if (priority == 2) {
+                            Q2.add(process);
+                        } else {
+                            System.out.println("Invalid priority. Process not added.");
+                        }
+                    }
+                    System.out.println("Process information entered successfully.");
+                    break;
+                case 2:
+                   
+                    break;
+                case 3:
+                    System.out.println("Exiting the program.");
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    break;
             }
-        }
+        } while (choice != 3);
+
     }
+
 }
