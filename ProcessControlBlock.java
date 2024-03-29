556d847a6f61860aa8f3489bdb17be737f9e346d
joud alnayel <146003258+joudalnayel@users.noreply.github.com>
2024-03-29 03:25:23 +0300

    Update ProcessControlBlock.java

--- ProcessControlBlock.java
+++ ProcessControlBlock.java
@@ -1,19 +1,75 @@
+public class PCB {
+	private String processID;
+    private int priority;
+    private int arrivalTime;
+    private int burstTime;
+    private int startTime;
+    private int terminationTime;
+    private int turnaroundTime;
+    private int waitingTime;
+    private int responseTime;
 
-public class ProcessControlBlock {
-    int processId,priority,arrivalTime,cpuBurstTime,startTime,terminationTime,turnAroundTime,waitingTime,responseTime;
-
-    public ProcessControlBlock(int processId, int priority, int arrivalTime, int cpuBurstTime,
-            int startTime, int terminationTime, int turnAroundTime, int waitingTime, int responseTime) {
-        this.processId = processId;
+    public PCB(String processID, int priority, int arrivalTime, int burstTime) {
+        this.processID = processID;
         this.priority = priority;
         this.arrivalTime = arrivalTime;
-        this.cpuBurstTime = cpuBurstTime;
+        this.burstTime = burstTime;
+    }
+
+    public String getProcessID() {
+        return processID;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
         this.startTime = startTime;
+    }
+
+    public int getTerminationTime() {
+        return terminationTime;
+    }
+
+    public void setTerminationTime(int terminationTime) {
         this.terminationTime = terminationTime;
-        this.turnAroundTime = turnAroundTime;
+    }
+
+    public int getTurnaroundTime() {
+        return turnaroundTime;
+    }
+
+    public void setTurnaroundTime(int turnaroundTime) {
+        this.turnaroundTime = turnaroundTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    public void setWaitingTime(int waitingTime) {
         this.waitingTime = waitingTime;
+    }
+
+    public int getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(int responseTime) {
         this.responseTime = responseTime;
     }
 
-    
 }
